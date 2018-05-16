c95a960b3ac4203eab22303273db760adad72743
Zhzer <zhz961101@gmail.com>
2018-05-16 18:05:51 +0800

    debug#2

--- src/hexdump.py
+++ src/hexdump.py
@@ -26,23 +26,25 @@ def hex_dump(src, length=16, encoding="utf8"):
     # return('\n'.join(result))
 
 
-def hex_Bin_dump(bin, length=16,start=0,end=None):
+def hex_Bin_dump(bin, length=16, start=0, end=None):
     result = []
     codeWidth = 2
 
     def hexRowGen(arr):
         # py3:int(2)/int(2)=>float(1)
         # py3:int(2)//int(2)=>int(1)
-        return ' '.join(hexArray[:length // 2]) + "-" + ' '.join(hexArray[length // 2:length]) if len(arr) > length // 2 else ' '.join(hexArray)
+        return ' '.join(hexArray[:length // 2]) + " - " + ' '.join(hexArray[length // 2:length]) if len(arr) > length // 2 else ' '.join(hexArray)
 
     for index in range(0, len(bin), length):
         block = bin[index:index + length]
-        hexArray = ["{hexValue:0>{hexLength}}".format(hexValue=hex(x)[2:].upper(), hexLength=codeWidth) for x in block]
+        hexArray = ["{hexValue:0>{hexLength}}".format(
+            hexValue=hex(x)[2:].upper(), hexLength=codeWidth) for x in block]
         hexRow = hexRowGen(hexArray)
         textArray = [chr(x) if 0x20 <= x <= 0x7f else '.' for x in block]
         text = ''.join([x for x in textArray])
-        result.append("{rowNum_hex:0>4}  {hexContent:<{hexRowlength}} {textContent}".format(rowNum_hex=hex(index//16)[2:].upper(), hexContent=hexRow, hexRowlength=length * (codeWidth + 1), textContent=text))
+        result.append("{rowNum_hex:0>4}  {hexContent:<{hexRowlength}} |{textContent:<{textlength}}|".format(rowNum_hex=hex(
+            index // 16)[2:].upper(), hexContent=hexRow, hexRowlength=length * (codeWidth + 1), textContent=text, textlength=length))
     if start > len(result):
-        start = len(result)-1
+        start = len(result) - 1
         end = None
     return('\n'.join(result[start:end]))
--- src/main.py
+++ src/main.py
@@ -4,39 +4,44 @@ import sys
 from .hexdump import *
 
 
-def _usage(err=None):
-    print("---Hexdump HELP DOC---")
+def print_usage(err=None):
+    print("\n\t---Hexdump HELP DOC---")
     print()
-    print(" Usage: hexdump.py -file log.md")
-    print(" -t or -text   : you can call hexdump with you printing")
-    print(" -f or -file   : you can call hexdump with you file")
-    print(" -h or -help   : show help doc for you")
-    print(" Examples      :")
-    print("                 hexdump.py -f run.py:0")
-    print("                 hexdump.py -f run.py:0:10")
-    print("                 hexdump.py -f run.py 20:100")
-    print("                 hexdump.py -f run.py ff:f")
-    print("                 hexdump.py -t hello word !!!!")
+    print("\t Usage:")
+    print("\t -t or -text <string or blank>  : \n\t\tyou can call hexdump with you printing")
+    print(
+        "\t -f or -file <filename> [<length>] : \n\t\tyou can call hexdump with you file")
+    print("\t -h or -help   : \n\t\tshow help doc for you")
+    print("\t Examples      :")
+    print("\t\thexdump.py -f run.py:0")
+    print("\t\thexdump.py -f run.py:0:10")
+    print("\t\thexdump.py -f run.py 20:100")
+    print("\t\thexdump.py -f run.py ff:f")
+    print("\t\thexdump.py -t hello word !!!!")
     if err:
         print()
-        print("---error massage----")
+        print("\t\t---error massage----")
         print(err)
-        print("--------------------")
+        print("\t\t--------------------")
         print()
     print()
-    print("------USE IT NOW------")
+    print("\t------USE IT NOW------")
     sys.exit(0)
 
 
 def printHex(src_str, start=0, end=None):
-    print(hex_dump(src_str) if not isinstance(src_str, bytes)else hex_Bin_dump(src_str, start=start, end=end))
+    print(hex_dump(src_str) if not isinstance(src_str, bytes)
+          else hex_Bin_dump(src_str, start=start, end=end))
     # print(hex_dump(src_str if isinstance(src_str,bytes) else str.encode()))
 
 
 def main():
+    default_length = 64
+    # hex(64) => 0x40
+    # -f <filename>:ff => -f <filename> ff:hex(dec(ff)+64)
     _argv = sys.argv[1:]
     if not len(_argv):
-        _usage()
+        print_usage()
     else:
         cmdn = _argv[0]
         cmdt = _argv[1:]
@@ -44,29 +49,31 @@ def main():
             printHex(" ".join(cmdt))
         elif cmdn in ["-f", "-file"]:
             start_str = "0"
-            end_str = "100" # 256
+            end_str = "100"  # 256
             filename = cmdt[0]
-            if len(cmdt)>1:
+            if len(cmdt) > 1:
                 if cmdt[1].find(":") is not -1:
                     start_str, end_str = cmdt[1].split(":")
                 else:
                     start_str = cmdt[1]
-                    end_str = str(int('0x'+ str(int('0x'+start_str,16)+100), 16))
+                    end_str = hex(int('0x' + start_str, 16) +
+                                  default_length)[2:]
             elif cmdt[0].find(":") is not -1:
                 args = cmdt[0].split(":")
                 filename = args[0]
                 start_str = args[1]
-                end_str = args[2] if len(args)>2 else str((int(start_str)+100))
-            start = int('0x'+ start_str if start_str is not "" else "0", 16)
-            end = int('0x'+ end_str, 16) + 1
+                end_str = args[2] if len(args) > 2 else hex(
+                    int('0x' + start_str, 16) + default_length)[2:]
+            start = int('0x' + start_str if start_str is not "" else "0", 16)
+            end = int('0x' + end_str, 16) + 1
             if start > end:
-                start,end = end-1,start+1
+                start, end = end - 1, start + 1
             try:
                 with open(filename, 'rb') as f:
-                    printHex(f.read(),int(start),int(end))
+                    printHex(f.read(), int(start), int(end))
             except (FileNotFoundError, IndexError) as e:
-                _usage(e)
+                print_usage(e)
         elif cmdn in ["-h", "-help"]:
-            _usage()
+            print_usage()
         else:
-            _usage()
+            print_usage()
